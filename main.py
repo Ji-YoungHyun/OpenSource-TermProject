286c3d4ea496a865d1e7f7fe98bd9b84d65ef5f2
KKanghh <attsky2@gmail.com>
2021-05-23 20:31:51 +0900

    Fix main

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import numpy as np
 import cv2
 import dlib
+import eyebrow
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 ALL = list(range(0, 68))
 RIGHT_EYEBROW = list(range(17, 22))
@@ -26,10 +27,14 @@ while True:
     img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     dets = detector(img_gray, 1)
-
     for face in dets:
-
-        shape = predictor(frame, face)
+        face_img = frame[face.top() - 30:face.bottom() + 30, face.left() - 30:face.right() + 30]
+        face_img = cv2.resize(face_img, (600, 600))
+        face_gray = cv2.cvtColor(face_img, cv2.COLOR_BGR2GRAY)
+        face_dets = detector(face_gray, 1)
+    
+    for face in face_dets:
+        shape = predictor(face_img, face)
 
         list_points = []
 
@@ -37,16 +42,18 @@ while True:
             list_points.append([p.x, p.y])
 
         list_points = np.array(list_points)
-
+        
         for i, pt in enumerate(list_points[index]):
 
             pt_pos = (pt[0], pt[1])
-            #cv2.circle(frame, pt_pos, 2, (0, 255, 0), -1)
-            cv2.putText(frame, str(i), pt_pos, cv2.FONT_HERSHEY_PLAIN, 0.5, (0, 255, 0))
-
-        #cv2.rectangle(frame, (face.left(), face.top()), (face.right(), face.bottom()), (0, 0, 255), 3) 얼굴범위 표시
+            cv2.putText(face_img, str(i), pt_pos, cv2.FONT_HERSHEY_PLAIN, 0.5, (0, 255, 0))
+        eyebrow.eyebrow(face_img)
+        cv2.imshow('face', face_img)
+        #cv2.rectangle(frame, (face.left(), face.top()), (face.right(), face.bottom()), (0, 0, 255), 3)
+    #cv2.putText(frame, 'hello', (300, 100), cv2.FONT_HERSHEY_PLAIN, 2.0, (0, 255, 0))    
     cv2.imshow('result', frame)
-
+    
+    #cv2.imshow('face', face_img)
     key = cv2.waitKey(0)
 
     if key == 27:
@@ -63,5 +70,5 @@ while True:
         index = MOUTH_OUTLINE + MOUTH_INNER
     elif key == ord('6'):
         index = JAWLINE
-
+cv2.destroyAllWindows()
 cap.release()
