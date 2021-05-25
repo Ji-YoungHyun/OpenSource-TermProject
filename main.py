a956e5f3ed95f67116c8ea00c56a945e352acc0f
KKanghh <attsky2@gmail.com>
2021-05-25 14:49:45 +0900

    Fix main

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import numpy as np
 import cv2
 import dlib
-import eyebrow
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
@@ -27,6 +26,9 @@ while True:
     img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     dets = detector(img_gray, 1)
+    if dets is None:
+        continue
+    
     for face in dets:
         face_img = frame[face.top() - 30:face.bottom() + 30, face.left() - 30:face.right() + 30]
         face_img = cv2.resize(face_img, (600, 600))
@@ -47,7 +49,6 @@ while True:
 
             pt_pos = (pt[0], pt[1])
             cv2.putText(face_img, str(i), pt_pos, cv2.FONT_HERSHEY_PLAIN, 0.5, (0, 255, 0))
-        eyebrow.eyebrow(face_img)
         cv2.imshow('face', face_img)
         #cv2.rectangle(frame, (face.left(), face.top()), (face.right(), face.bottom()), (0, 0, 255), 3)
     #cv2.putText(frame, 'hello', (300, 100), cv2.FONT_HERSHEY_PLAIN, 2.0, (0, 255, 0))    
