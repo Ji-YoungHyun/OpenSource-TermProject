4d9997c36d0c20081b4350811cf97c8d6adf2f79
Ji-YoungHyun <79207647+Ji-YoungHyun@users.noreply.github.com>
2021-05-27 13:48:21 +0900

    Update main.py
    각 부위 추출해서 띄워주는
    split_face(image, detector, predictor) 함수 추가했습니다.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import numpy as np
 import cv2
 import dlib
+import imutils
+from imutils import face_utils
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+image = cv2.imread('C:/Users/USER/Desktop/1.jpg')
+image = imutils.resize(image, width=500)
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
@@ -14,17 +18,44 @@ RIGHT_EYE = list(range(36, 42))
 LEFT_EYE = list(range(42, 48))
 NOSE = list(range(27, 36))
 MOUTH_OUTLINE = list(range(48, 61))
-MOUTH_INNER = list(range(61,68))
+MOUTH_INNER = list(range(61, 68))
 JAWLINE = list(range(0, 17))
 
 index = ALL
 
+def split_face(image, detector, predictor):
+    img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    rects = detector(img_gray, 1)
+
+    for (i, rect) in enumerate(rects):
+        shape = predictor(img_gray, rect)
+        shape = face_utils.shape_to_np(shape)
+
+        for (name, (i, j)) in face_utils.FACIAL_LANDMARKS_IDXS.items():
+            clone = image.copy()
+            cv2.putText(clone, name, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255),2)
+
+            for (x, y) in shape[i:j]:
+                cv2.circle(clone, (x, y), 1, (0, 0, 255), -1)
+
+            (x, y, w, h) = cv2.boundingRect(np.array([shape[i:j]]))
+
+            roi = image[y:y + h, x:x + w]
+            roi = imutils.resize(roi, width=250, inter = cv2.INTER_CUBIC)
+
+            cv2.imshow("ROI", roi)
+            cv2.imshow("Image", clone)
+            cv2.waitKey(0)
+
+    output = face_utils.visualize_facial_landmarks(image, shape)
+    cv2.imshow("Image", output)
+    cv2.waitKey(0)
+
+split_face(image, detector, predictor)
+
 while True:
-
     ret, frame = cap.read()
-
     img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
     dets = detector(img_gray, 1)
 
     for face in dets:
@@ -32,7 +63,7 @@ while True:
         face_img = cv2.resize(face_img, (600, 600))
         face_gray = cv2.cvtColor(face_img, cv2.COLOR_BGR2GRAY)
         face_dets = detector(face_gray, 1)
-    
+
         for face in face_dets:
             shape = predictor(face_img, face)
 
@@ -42,17 +73,16 @@ while True:
                 list_points.append([p.x, p.y])
 
             list_points = np.array(list_points)
-            
-            for i, pt in enumerate(list_points[index]):
 
+            for i, pt in enumerate(list_points[index]):
                 pt_pos = (pt[0], pt[1])
                 cv2.putText(face_img, str(i), pt_pos, cv2.FONT_HERSHEY_PLAIN, 0.5, (0, 255, 0))
             cv2.imshow('face', face_img)
-        #cv2.rectangle(frame, (face.left(), face.top()), (face.right(), face.bottom()), (0, 0, 255), 3)
-    #cv2.putText(frame, 'hello', (300, 100), cv2.FONT_HERSHEY_PLAIN, 2.0, (0, 255, 0))    
+        # cv2.rectangle(frame, (face.left(), face.top()), (face.right(), face.bottom()), (0, 0, 255), 3)
+    # cv2.putText(frame, 'hello', (300, 100), cv2.FONT_HERSHEY_PLAIN, 2.0, (0, 255, 0))
     cv2.imshow('result', frame)
-    
-    #cv2.imshow('face', face_img)
+
+    # cv2.imshow('face', face_img)
     key = cv2.waitKey(1)
 
     if key == 27:
@@ -69,5 +99,6 @@ while True:
         index = MOUTH_OUTLINE + MOUTH_INNER
     elif key == ord('6'):
         index = JAWLINE
+
 cv2.destroyAllWindows()
 cap.release()
